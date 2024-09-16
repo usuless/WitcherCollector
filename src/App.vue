73077c8ef1d962c56340667a9a8914000137b840
usuless <61857692+usuless@users.noreply.github.com>
2024-09-16 21:11:03 +0200

    data fix

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,71 @@
 <script setup lang="ts">
+import {reactive} from "vue";
 import Head from './components/Head.vue';
-import data from "./assets/data/cards.json"
+import cards from "./assets/data/cards.json"
+import Moje from './components/moje.vue';
 
-let cardsData = data
+const localStorageKey = 'cardsData'
+
+const saveCards = () => {
+   localStorage.setItem(localStorageKey, JSON.stringify(ownedCards))
+}
+
+const loadCards = () => {
+   const cards = localStorage.getItem(localStorageKey);
+
+   if(!cards) return {}
+   try {
+      return JSON.parse(cards)
+      
+   } catch {
+   console.log("Error parsing localstorage data")
+      return {}
+   }
+}
+
+const ownedCards = reactive<Record<number, boolean>>(loadCards())
+
+const isCardOwned = (id: number) => {
+   if(!(id in ownedCards)) return false
+   
+   return ownedCards[id];
+}
+
+const toggleCard = (id: number) => {
+   if(!(id in ownedCards)) {
+      ownedCards[id] = true;
+   saveCards()
+      return;
+   }
+   
+   ownedCards[id] = !ownedCards[id]
+   
+   saveCards()
+}
+
+
+
 </script>
-
 <template>
+   <Moje/>
  <Head/>
- <div v-for="card in cardsData" class="">
-    <p>{{card.Card}}</p>
- </div>
+   <div class="grid grid-cols-6 gap-4 w-full">
+       <div v-for="card in cards" class="border border-transparent" :class="{owned: isCardOwned(card.id)}" :key="card.id">
+
+          <img :src="card.image">
+          <p class="">{{card.card}}</p>
+          <p class="">{{card.location}}</p>
+          <p class="">{{card.deck}}</p>
+          <button @click="toggleCard(card.id)">Toggle</button>
+       </div>
+   </div>
 </template>
 
-<style scoped>
+<style lang="postcss" scoped>
+
+.owned {
+   @apply bg-red-500 border-red-500;
+}
+
 
 </style>
---
+++ src/components/moje.vue
@@ -0,0 +1,32 @@
+<script setup>
+</script>
+<template>
+    <div class="flex w-8/12 justify-between bg-green-600 text-black clippy my-2 p-1 px-10">
+        <p>nazwa karty</p>
+        <p>lokalizacja</p>
+        <p>talia</p>
+        <input type="checkbox">        
+    </div>
+</template>
+<style scoped>
+
+ .clippy {
+    clip-path: polygon(0 1%, 97% 0, 100% 100%, 0% 100%);
+ }
+ /*
+ .clippy:hover {
+    animation: 1s scale;
+ }
+
+ @keyframes scale {
+    0% {
+        transform: scale(1) translateX(0px);
+
+    }
+
+    100% {
+        transform: scale(1.1) translateX(50px);
+    }
+    
+ } */
+</style>
