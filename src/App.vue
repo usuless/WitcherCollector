cc84b35645e9b20bfbe58aafbcff62f682f91149
usuless <61857692+usuless@users.noreply.github.com>
2024-09-16 18:05:42 +0200

    data fix

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 import Head from './components/Head.vue';
+import data from "./assets/data/cards.json"
 
+let cardsData = data
 </script>
 
 <template>
  <Head/>
+ <div v-for="card in cardsData" class="">
+    <p>{{card.Card}}</p>
+ </div>
 </template>
 
 <style scoped>
