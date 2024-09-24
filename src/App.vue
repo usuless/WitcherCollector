d80e59cfd4c673b11b31e3682bf02418e7908ab0
usuless <61857692+usuless@users.noreply.github.com>
2024-09-24 03:03:30 +0200

    IT WORKS!!!!!!!!!!!!!!!!!!

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,32 @@ import cards from "./assets/data/cards.json"
 import Card from './components/Card.vue';
 import SearchSection from "./components/SearchSection.vue";
 import type { Ref } from "vue";
-import { computed, ref } from "vue";
+import { computed, ref, watch } from "vue";
+import { refDebounced} from '@vueuse/core';
 
 let searchLocation: Ref<string> = ref('')
 let searchName:Ref<string>=ref('')
+let deckName:Ref<string>=ref('')
 
-const filteredLocation = computed(() => {
+   const filteredLocation = computed(() => {
    return cards.filter(item => item.location.toLowerCase().includes(searchLocation.value.toLowerCase()))
 })
 
-const sumOfFilters = computed(() => {
+   const filteredNames = computed(() => {
    return filteredLocation.value.filter(item => item.card.toLowerCase().includes(searchName.value.toLowerCase()))
 })
+
+const finalFilter = ref(cards)
+
+const debounced = refDebounced(filteredNames, 700)
+
+watch(debounced, () => finalFilter.value = filteredNames.value)
 
 </script>
 <template>
    <SearchSection :data="cards" 
-   @name="(name) => searchName = name" 
-   @location="(location) => searchLocation = location"/>
-   <Card :card="card" v-for="card in sumOfFilters" :key="card.id"/>
+   v-model:location="searchLocation"
+   v-model:name="searchName"
+   v-model:deck="deckName"/>
+   <Card :card="card" v-for="card in finalFilter" :key="card.id"/>
 </template>
--- src/components/SearchSection.vue
+++ src/components/SearchSection.vue
@@ -1,56 +1,44 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import type { Ref } from 'vue';
+import { defineModel} from 'vue';
 
-const location: Ref<string> = ref('')
-const name: Ref<string> = ref('')
-
-const emit = defineEmits<{
-  (e: 'location', location: string): void,
-  (e: 'name', name: string): void,
-  (e: 'deck', deck: any): void
-}>()
-
-let deckChange = (event: any):void => {
-    emit('deck', event.target.id)
-}
-
-/* idk what to do :((( */
+let locationModel = defineModel<string>('location', {required:true});
+let nameModel = defineModel<string>('name', {required:true});
+let deckModel = defineModel<string>('deck', {required:true})
 
 </script>
 <template>
     <div class="flex items-center">
         <div class="mr-5">
-            <input v-model="name" @input="emit('name', name)" class="p-3 rounded-3xl" name="card" type="text">
+            <input v-model="nameModel" class="p-3 rounded-3xl" name="card" type="text">
             <label class="ml-3" for="card">Jaka karta?</label>
         </div>
         <div class="mr-5">
-            <input v-model="location" @input="emit('location', location)" class="p-3 rounded-3xl" name="location" type="text">
+            <input v-model="locationModel" class="p-3 rounded-3xl" name="location" type="text">
             <label class="ml-3" for="location">Gdzie szukasz?</label>
         </div>
         <div class="grid grid-cols-3 grid-row-2">
             <div class="content-between mx-2">
-                <input @input="event => deckChange(event)" class="justify-self-center" type="radio" id="Neutralne" name="deck">
+                <input v-model="deckModel" class="justify-self-center" type="radio" id="Neutralne" name="deck">
                 <p>Neutralne</p>
             </div>
             <div class="content-between mx-2">
-                <input @input="event => deckChange(event)" class="justify-self-center" type="radio" id="Potwory" name="deck">
+                <input v-model="deckModel" class="justify-self-center" type="radio" id="Potwory" name="deck">
                 <p>Potwory</p>
             </div>
             <div class="content-between mx-2">
-                <input @input="event => deckChange(event)" class="justify-self-center" type="radio" id="Skellige" name="deck">
+                <input v-model="deckModel" class="justify-self-center" type="radio" id="Skellige" name="deck">
                 <p>Skellige</p>
             </div>
             <div class="content-between mx-2">
-                <input @input="event => deckChange(event)" class="justify-self-center" type="radio" id="Nilfgaard" name="deck">
+                <input v-model="deckModel" class="justify-self-center" type="radio" id="Nilfgaard" name="deck">
                 <p>Nilfgaard</p>
             </div>
             <div class="content-between mx-2">
-                <input @input="event => deckChange(event)" class="justify-self-center" type="radio" id="Scoia'tael" name="deck">
+                <input v-model="deckModel" class="justify-self-center" type="radio" id="Scoia'tael" name="deck">
                 <p>Scoia'tael</p>
             </div>
             <div class="content-between mx-2">
-                <input @input="event => deckChange(event)" class="justify-self-center" type="radio" id="Królestwa Północy" name="deck">
+                <input v-model="deckModel" class="justify-self-center" type="radio" id="Królestwa Północy" name="deck">
                 <p>Królestwa Płn.</p>
             </div>
         </div>
