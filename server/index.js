8adccee8e213f6f6936d661ad9a4e3ae0d959a95
usuless <61857692+usuless@users.noreply.github.com>
2026-01-10 13:51:56 +0100

    fired

---
+++ server/index.js
@@ -0,0 +1,13 @@
+import fastify from "fastify";
+const server = fastify();
+server.get("/ping", async (req, repl) => {
+    return "pong\n";
+});
+server.listen({ port: 8000 }, (err, address) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log(`server on ${address}`);
+});
+//# sourceMappingURL=index.js.map
