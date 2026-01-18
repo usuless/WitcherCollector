220cc2a75e0e123701e9c5cdacc2c284005ff331
usuless <61857692+usuless@users.noreply.github.com>
2026-01-18 18:34:38 +0100

    database

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,15 @@
 import Fastify from "fastify";
 import authRoutes from "./routes/auth.js";
 import apiRoutes from "./routes/api.js";
+import fastifyPostgres from "@fastify/postgres";
 
 export const buildApp = (opts = {}) => {
   const app = Fastify(opts);
 
   app.register(authRoutes, { prefix: "/auth" });
   app.register(apiRoutes, { prefix: "/api" });
-
+  app.register(fastifyPostgres, {
+    connectionString: "postgres://postgres:5173@localhost:5432/Witcher",
+  });
   return app;
 };
--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -4,4 +4,21 @@ export default async function apiRoutes(fastify: FastifyInstance) {
   fastify.get("/", async (request, reply) => {
     return { Hello: "World" };
   });
+
+  fastify.get("/cards", async (request, reply) => {
+    let client;
+    try {
+      client = await fastify.pg.connect();
+      const { rows } = await client.query(
+        "SELECT * FROM gwent_cards ORDER BY id ASC",
+      );
+
+      return rows;
+    } catch (err) {
+      fastify.log.error(err);
+      return reply.status(500).send({ error: "Błąd bazy danych" });
+    } finally {
+      if (client) client.release();
+    }
+  });
 }
